fda2f8ddf1b49a8e9142bc751d9379414e1830f5
Lorrany Abrantes <abranteslorrany@outlook.com>
2022-05-01 22:19:27 -0300

    add `skills` and `certificates` on `candidate` data

--- src/Get/Candidate.js
+++ src/Get/Candidate.js
@@ -8,15 +8,32 @@ export const GetCandidate = (params, callback) => {
       rows: [candidate],
     } = result;
 
-    callback({
-      id: candidate[0],
-      name: candidate[1],
-      cpf: candidate[2],
-      email: candidate[3],
-      phone: candidate[4],
-      bithdate: candidate[5],
-      gender: candidate[6],
-    });
+    const selectSkill = `SELECT * FROM TB_BRQ_C_H WHERE cd_candidato=${params.id}`;
+
+    const dataSkill = (resultSkill) => {
+      const { rows: skills } = resultSkill;
+
+      const selectCertificate = `SELECT * FROM TB_BRQ_C_C WHERE cd_candidato=${params.id}`;
+
+      const dataCertificate = (resultCertificate) => {
+        const { rows: certificates } = resultCertificate;
+        callback({
+          id: candidate[0],
+          name: candidate[1],
+          cpf: candidate[2],
+          email: candidate[3],
+          phone: candidate[4],
+          bithdate: candidate[5],
+          gender: candidate[6],
+          skills: skills.map((item) => item[1]),
+          certificates: certificates.map((item) => item[1]),
+        });
+      };
+
+      execute(selectCertificate, dataCertificate);
+    };
+
+    execute(selectSkill, dataSkill);
   };
 
   execute(select, data);
